e9c4a5e2e2efd11d428fe027a85aff2986350c3c
ferdie bergado <ferdiebergado@gmail.com>
2022-07-05 15:46:19 +0800

    Enabled autoLoadEntities

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,7 +4,6 @@ import { TypeOrmModule } from "@nestjs/typeorm";
 import { AppController } from "./app.controller";
 import { AppService } from "./app.service";
 import { UsersModule } from "./users/users.module";
-import { User } from "./users/entities/user.entity";
 
 @Module({
   imports: [
@@ -16,7 +15,7 @@ import { User } from "./users/entities/user.entity";
       username: process.env.DATABASE_USER || "user",
       password: process.env.DATABASE_PASS,
       database: process.env.DATABASE_NAME || "test",
-      entities: [User],
+      autoLoadEntities: true,
       synchronize: true,
     }),
     UsersModule,
